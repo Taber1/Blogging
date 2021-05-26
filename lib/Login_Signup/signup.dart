df87aa8453e73bbf0e182ae26acecd3a5021856c
Taber Bin Zameer <tbzameer@gmail.com>
2021-05-26 11:53:24 +0500

    Some Changes

--- lib/Login_Signup/signup.dart
+++ lib/Login_Signup/signup.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:project/Menu/categories.dart';
@@ -11,135 +10,129 @@ class Sin extends StatefulWidget {
 }
 
 class _SinState extends State<Sin> {
-
- 
-
   @override
- Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Form(
-          child: Padding(
+        child: Padding(
           padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
           child: SingleChildScrollView(
-                child: Column(
+            child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  SizedBox(height: 30.0,),
+                  SizedBox(
+                    height: 30.0,
+                  ),
                   Container(
-                    margin:  EdgeInsets.only(top:0.0),
-                    child:Text('R E G I S T R A T I O N', 
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.green[700]
-                    )
-                    ),
-                    
+                    margin: EdgeInsets.only(top: 0.0),
+                    child: Text('R E G I S T R A T I O N',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green[700])),
                   ),
-                  SizedBox(height: 30.0,),
+                  SizedBox(
+                    height: 30.0,
+                  ),
                   Material(
-                    
                     elevation: 2,
                     child: TextFormField(
-                        decoration: InputDecoration(    
-                          enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color:Colors.grey[300]),
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        ),
-                        prefixIcon:Icon(Icons.person),
-                        hintText: 'Username',
-                      )
-                    ),
+                        decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[300]),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      prefixIcon: Icon(Icons.person),
+                      hintText: 'Username',
+                    )),
                   ),
-                 SizedBox(height: 10.0,),
+                  SizedBox(
+                    height: 10.0,
+                  ),
                   Material(
-                    
                     elevation: 2,
                     child: TextFormField(
-                     
-                        decoration: InputDecoration(    
-                          enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color:Colors.grey[300]),
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        ),
-                        prefixIcon:Icon(Icons.email),
-                        hintText: 'Email',
-                      )
-                    ),
+                        decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[300]),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      prefixIcon: Icon(Icons.email),
+                      hintText: 'Email',
+                    )),
                   ),
-                  SizedBox(height: 10.0,),
+                  SizedBox(
+                    height: 10.0,
+                  ),
                   Material(
-                    
                     elevation: 2,
                     child: TextFormField(
                         obscureText: true,
-                        
-                        decoration: InputDecoration(    
+                        decoration: InputDecoration(
                           enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color:Colors.grey[300]),
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                          
-                        ),
-                        
-                        prefixIcon:Icon(Icons.lock),
-                        hintText: 'Password',
-                      )
-                    ),
+                            borderSide: BorderSide(color: Colors.grey[300]),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                          ),
+                          prefixIcon: Icon(Icons.lock),
+                          hintText: 'Password',
+                        )),
                   ),
-
                   Container(
-                    
-                    margin:EdgeInsets.only(top:40.0),
-                    height: 45.0,
-                    child: RaisedButton(onPressed: (){
-                      Navigator.push(
-                        context, MaterialPageRoute(builder: (context) => Categories() ));
-                    },
-                    color: Colors.green[700],
-                    shape: RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(30.0)),
-                    elevation: 2,
-                    child: Text('REGISTER',
-                  
-                    style:TextStyle(color: Colors.white)
-                ),
-                    )
-                  ),
+                      margin: EdgeInsets.only(top: 40.0),
+                      height: 45.0,
+                      child: RaisedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Categories()));
+                        },
+                        color: Colors.green[700],
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(30.0)),
+                        elevation: 2,
+                        child: Text('REGISTER',
+                            style: TextStyle(color: Colors.white)),
+                      )),
                   Divider(
-                    height:40.0,
-                    thickness: 0.70,
-                    color:Colors.green[700]
+                      height: 40.0, thickness: 0.70, color: Colors.green[700]),
+                  SizedBox(
+                    height: 10,
                   ),
-                  SizedBox(height: 10,),
-                    Container(
-                  width: 250.0,
+                  Container(
+                    width: 250.0,
                     child: Align(
-                  alignment: Alignment.center,
-                  child: RaisedButton(
-                    onPressed:(){},
-                    shape: RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(30.0)),
-                    color: Color(0xffffffff),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                      Icon(FontAwesomeIcons.google,color: Colors.green,),
-                      SizedBox(width:10.0),
-                      Text(
-                      'Sign in with Google',
-                      style: TextStyle(color: Colors.black,fontSize: 18.0),
+                      alignment: Alignment.center,
+                      child: RaisedButton(
+                        onPressed: () {},
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(30.0)),
+                        color: Color(0xffffffff),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              FontAwesomeIcons.google,
+                              color: Colors.green,
+                            ),
+                            SizedBox(width: 10.0),
+                            Text(
+                              'Sign in with Google',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 18.0),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                    ],),),),),
-               
-                  
-                ]
-              ),
+                  ),
+                ]),
           ),
         ),
       ),
-      resizeToAvoidBottomPadding: false,
       resizeToAvoidBottomInset: false,
     );
   }
